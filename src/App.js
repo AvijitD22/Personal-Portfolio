9305b63443806a02c8e9c23c273fbb31e52e8269
AvijitD22 <avijitd22@yahoo.com>
2024-03-09 18:41:57 +0530

    Remove useless routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Screens/Home";
-import About from "./Screens/About";
-import Projects from "./Screens/Projects";
-import Blogs from "./Screens/Blogs";
-import Contact from "./Screens/Contact";
 import PageNotFound from "./Screens/PageNotFound";
 
 const App = () => {
@@ -14,10 +10,6 @@ const App = () => {
       <Router>
         <Routes>
           <Route path={"/"} element={<Home />} />
-          <Route path={"/about"} element={<About />} />
-          <Route path={"/projects"} element={<Projects />} />
-          <Route path={"/blogs"} element={<Blogs />} />
-          <Route path={"/contact"} element={<Contact />} />
           <Route path={"*"} element={<PageNotFound />} />
         </Routes>
       </Router>
