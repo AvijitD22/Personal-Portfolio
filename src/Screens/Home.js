fcadaa3a778b0f851243741ce8a9a9eece396468
AvijitD22 <avijitd22@yahoo.com>
2024-03-09 18:40:49 +0530

    Add feature: Add To Top Button
    - Load on screen scroll
    - Navigate to heading section on clicked

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import HeadingSection from "./Components/HeadingSection";
 import AboutSection from "./Components/AboutSection";
@@ -10,6 +10,21 @@ import FooterSection from "./Components/FooterSection";
 import styles from "./Home.module.css";
 
 const Home = () => {
+  const [show, setShow] = useState(false);
+
+  const navbarTransition = () => {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", navbarTransition);
+    return () => window.removeEventListener("scroll", navbarTransition);
+  }, []);
+
   return (
     <div className={styles.home}>
       <Navbar />
@@ -20,6 +35,20 @@ const Home = () => {
       <ContactSection />
       <BlogSection />
       <FooterSection />
+      {show ? (
+        <a href="#heading">
+          <button
+            data-aos="fade-up"
+            data-aos-delay="200"
+            data-aos-duration="1000"
+            className={styles.to_top}
+          >
+            ^
+          </button>
+        </a>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
